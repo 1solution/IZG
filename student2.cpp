f42eb078414bb3eb870d43f0e75f78955444e329
xplsek03 <Michal.Plsek@seznam.cz>
2019-03-07 17:47:57 +0100

    Update student2.cpp

--- student2.cpp
+++ student2.cpp
@@ -45,33 +45,33 @@ using namespace std;
 ////////////////////////////////////////////////////////////////////////////////
 // Ukol za 2b
 ////////////////////////////////////////////////////////////////////////////////
-#define FRAC_BITS 8
-void drawLine (int x1, int y1, int x2, int y2) {
-	int dx = (x2-x1);
-	int dy = (y2-y1);
+#define FRAC_BITS 8 // DDA fixed point
+void drawLine (int x1, int y1, int x2, int y2) { // zadany dva body, ktere definuji primku
+	int dx = (x2-x1); // vzdalenost na ose x
+	int dy = (y2-y1); // vzdalenost na ose y
 
-    if(ABS(dy)>ABS(dx)) {
-		SWAP(x1, y1);
-		SWAP(x2, y2);
-	}
-	if(x1>x2) {
-		SWAP(x1, x2);
-		SWAP(y1, y2);
-    }
-
-    if(!dx && !dy) {
-		putPixel(x1, y1, COLOR_GREEN);
+	if(!dx && !dy) { // oba body zadane stejne, primku tvori pouze bod
+		putPixel(x1, y2, COLOR_GREEN);
 		return;
-	}
+    	}
+	
+    	if(ABS(dy)>ABS(dx)) { // usecka klesa moc rychle, vymenit x a y souradnice
+		SWAP(x1, y1); // timhle se dostane do spravne poloviny kvadrantu
+		SWAP(x2, y2); // pak pri zapisu do bufferu taky prohodit
+    	}
+    	if(x1>x2) { // usecka smeruje zprava doleva, staci vymenit body mezi sebou
+		SWAP(x1, x2); 
+		SWAP(y1, y2);
+    	}
 
 	int y = y1 << FRAC_BITS;
 	int k = (dy << FRAC_BITS) / dx;
 
 	for (int x = x1; x <= x2; x++) {
         if(ABS(dy)>ABS(dx))
-            putPixel(y >> FRAC_BITS,x , COLOR_GREEN);
+            putPixel(y >> FRAC_BITS,x , COLOR_GREEN); // znovu prohodit v pripade ze usecka byla ve spatne casti kvadrantu
         else
-            putPixel( x, y >> FRAC_BITS, COLOR_GREEN);
+            putPixel( x, y >> FRAC_BITS, COLOR_GREEN); // nemenit, ani v pripade kdy usecka smerovala zprava doleva (body uz jsme mezi sebou prohodili nebo to neni potreba)
         y += k;
 	}
 }
@@ -79,8 +79,8 @@ void drawLine (int x1, int y1, int x2, int y2) {
 ////////////////////////////////////////////////////////////////////////////////
 // Ukol za 1b
 ////////////////////////////////////////////////////////////////////////////////
-void put8PixelsOfCircle(int x, int y, int s1, int s2, S_RGBA color) {
-    putPixel(-x,-y,color);
+void put8PixelsOfCircle(int x, int y, S_RGBA color) {
+    putPixel(-x,-y,color); // vykresleni osmi bodu podle zadani
     putPixel(-x,y,color);
     putPixel(x,-y,color);
     putPixel(x,y,color);
@@ -100,7 +100,7 @@ void drawCircle (int s1, int s2, int R)
 	int x = 0, y = R;
 	int P = 1-R, X2 = 3, Y2 = 2*R-2;
 	while (x <= y) {
-		put8PixelsOfCircle(x, y, s1, s2, col);
+		put8PixelsOfCircle(x, y, col);
 		if (P >= 0) {
 			P += -Y2;
 			Y2 -= 2;
